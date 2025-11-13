04ce7b996be606458a8cd035cb99006fc4cc2a7e
Advitya Dua <advityadua@Advityas-MacBook-Air.local>
2025-11-14 00:42:34 +0530

    Updated some stuff-7

--- api/settings.py
+++ api/settings.py
@@ -43,8 +43,8 @@ AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
