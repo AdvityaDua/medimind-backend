c88d3874cea6a35c67960cd6aa69667635c9b685
Advitya Dua <advityadua@Advityas-MacBook-Air.local>
2025-11-14 00:49:29 +0530

    Updated some stuff-10

--- api/settings.py
+++ api/settings.py
@@ -42,8 +42,8 @@ CORS_ALLOWED_ORIGINS = [
 AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
